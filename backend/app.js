0e7b22f6629ae4c1e314366ecdde24e4c9c9c8bc
lisahoong <lhoong94@gmail.com>
2017-09-16 20:42:54 -0400

    added express and body parser

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,30 @@
 // app js
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+var config = {
+  apiKey: process.env.sh,
+  authDomain: "projectId.firebaseapp.com",
+  databaseURL: "https://databaseName.firebaseio.com",
+  storageBucket: "bucket.appspot.com"
+};
+firebase.initializeApp(config);
+
+// Get a reference to the database service
+var database = firebase.database();
+
+
+function writeUserData(userId, name, email, imageUrl) {
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  });
+}
